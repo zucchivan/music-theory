13eb7a6459f89c850e13136b4b6e4ebd66058476
Charney Kaye <charney@outright.io>
2016-03-20 15:18:05 -0400

    godoc

--- music.go
+++ music.go
@@ -1,4 +1,19 @@
-// Package main implements a command-line utility for music
+// Opinionated models of the building blocks of music theory
+//
+// The command-line utility `music.go` will demo the Music libraries:
+//
+//     go run music.go -k "C minor 7"
+//
+// This will output:
+//
+//     root: C
+//     major: false
+//     minor: true
+//     tones:
+//       3: D#
+//       5: G
+//       7: A#
+//
 package main
 
 import (
